660d55059d32ad36c0eb25ff6031728b89d1e94d
tokizaki <tokizaki95@i.softbank.js>
2022-12-06 23:11:09 +0900

    feat: 掲示板のレイアウト実装（データはダミーを使用）

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,11 +1,34 @@
 import Link from 'next/link'
 import React from 'react'
+import DummyData from './DummyData'
 
 const Board = () => {
+  const postData = DummyData()
   return (
     <div>
-      <div>掲示板</div>
-      <Link href='/'>メニューへ</Link>
+      <Link style={{ paddingLeft: '20px' }} href='/'>メニューへ</Link>
+      <div style={{ padding: '20px' }}>
+        <div>
+          <span>タイトル：</span>
+          <input type='text'></input>
+          <span style={{ marginLeft: '5px' }}>投稿内容：</span>
+          <input type='text'></input>
+          <button className='btn btn-primary' style={{ margin: '5px' }}>投稿</button>
+        </div>
+        <hr></hr>
+        <div>
+          {postData.map((v, k)=>(
+            <div key={k} className='card'>
+              <div className='card-header'>{v.date}</div>
+              <div className='card-body'>
+                <h5 className='card-title'>{v.title}</h5>
+                <p className='card-text'>{v.content}</p>
+                <button className='btn btn-primary'>削除</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
---
+++ components/DummyData.ts
@@ -0,0 +1,11 @@
+const DummyData = () => {
+    const dummyData = [
+        {date:'2022/12/01 12:00', title: 'テスト01', content: 'next js学習中です。'},
+        {date:'2022/12/01 12:10', title: 'テスト02', content: 'reactから勉強した方がよいです。'},
+        {date:'2022/12/01 12:20', title: 'テスト03', content: 'typescriptをやった方がよいです。'},
+        {date:'2022/12/01 13:00', title: 'テスト04', content: 'webの基本から学んだ方がよいです。'},
+    ]
+    return dummyData
+}
+
+export default DummyData
