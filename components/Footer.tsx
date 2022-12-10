e77467b091ab8103a1042750a61984ee8fb56bca
tokizaki <tokizaki95@i.softbank.js>
2022-12-10 23:18:00 +0900

    feat: フッター修正

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   // my-4 上下のmarginを1.5rem (margin:枠の外側部分)
-  return <div className='text-center h6 my-4'>フッター</div>
+  return <div className='text-center h6 my-4'>sample board app</div>
 }
 
 export default Footer
