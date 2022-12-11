dad1fdd3dc4ca6fe9f964af8fc82ea88f5870ff3
tokizaki <tokizaki95@i.softbank.js>
2022-12-11 10:44:58 +0900

    feat: apikeyを環境変数で指定するように修正

--- components/Firebase.ts
+++ components/Firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCBVLL00EDwXm94MDHvrK-TWmLCwzjv3aw',
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
   authDomain: 'sample-next-js-app.firebaseapp.com',
   projectId: 'sample-next-js-app',
   storageBucket: 'sample-next-js-app.appspot.com',
