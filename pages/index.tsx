dc00452e8ef2f93b20a64638eecc4679cff474e6
tokizaki <tokizaki95@i.softbank.js>
2022-12-11 11:02:46 +0900

    feat: メニュー画面のレイアウト調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <div>メニュー画面</div>
-      <Link href='/board'>掲示板へ</Link>
+    <div style={{ padding: '20px' }} className='px-3 text-center'>
+      <div className='display-4'>メニュー画面</div>
+      <div style={{ marginTop: '50px', marginBottom: '50px' }}>
+        <Link href='/board' className='display-6'>
+          掲示板へ
+        </Link>
+      </div>
     </div>
   )
 }
