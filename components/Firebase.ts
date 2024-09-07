5c649707ade9f16125d7a7d874aeb9e2d1ca7d6f
tokizaki <tokizaki@tdc.co.jp>
2024-09-07 17:07:30 +0900

    chore: リポジトリ名変更に対応 / firebaseとgithubを連携しcicd作成 / 追加されたissueを自動でProjectに追加するGitHub Actions作成

--- components/Firebase.ts
+++ components/Firebase.ts
@@ -3,11 +3,11 @@ import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: 'sample-board-app.firebaseapp.com',
-  projectId: 'sample-board-app',
-  storageBucket: 'sample-board-app.appspot.com',
-  messagingSenderId: '849385797341',
-  appId: '1:849385797341:web:243c481302bda8b47ecd34',
+  authDomain: 'sample-board-nextjs-app.firebaseapp.com',
+  projectId: 'sample-board-nextjs-app',
+  storageBucket: 'sample-board-nextjs-app.appspot.com',
+  messagingSenderId: '994951739434',
+  appId: '1:994951739434:web:41cb056919234f6b7c2b53',
 }
 
 const app = initializeApp(firebaseConfig)
